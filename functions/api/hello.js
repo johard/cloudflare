b332821986a5ca1df5b6ec8192277246b8cd56db
Johan Ardlin <j.ardlin@gmail.com>
2023-05-24 14:40:07 +0200

    Commit

--- functions/api/hello.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  console.log(context)
-  const json = {
-    data: 'Hello World'
-  }
-  return new Response(JSON.stringify(json), { headers: { "Content-Type": "application/json" } })
-}
-
